e9dd4fc62ed552bd6ffbd3b05fbac9dc5b4bfa1d
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2023-04-04 13:56:57 -0300

    Home page

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:homemade_app/app/core/ui/widgets/delivery_app_bar.dart';
+import 'package:homemade_app/app/models/product_model.dart';
+import 'package:homemade_app/app/pages/home/widgets/delivery_product_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,10 +9,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+      appBar: DeliveryAppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => DeliveryProductTile(
+                product: ProductModel(
+                  id: 0,
+                  name: 'Lanche X',
+                  description:
+                      'Lanche acompanha pão, blend, mussarela e maionese',
+                  price: 15.5,
+                  image:
+                      'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800',
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }

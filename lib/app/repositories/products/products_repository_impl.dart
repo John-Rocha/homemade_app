e90270f837099fe3992d00f8d911faf42dace602
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2023-04-04 11:58:37 -0300

    Repository para busca de produtos

---
+++ lib/app/repositories/products/products_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:homemade_app/app/core/exceptions/repository_exception.dart';
+import 'package:homemade_app/app/core/rest_client/custom_dio.dart';
+import 'package:homemade_app/app/models/product_model.dart';
+
+import './products_repository.dart';
+
+class ProductsRepositoryImpl implements ProductsRepository {
+  final CustomDio dio;
+
+  ProductsRepositoryImpl({required this.dio});
+
+  @override
+  Future<List<ProductModel>> findAllProducts() async {
+    try {
+      final result = await dio.unauth().get('/products');
+      return result.data
+          .map<ProductModel>(
+            (p) => ProductModel.fromMap(p),
+          )
+          .toList();
+    } on DioError catch (e, s) {
+      log('Erro ao buscar produtos', error: e, stackTrace: s);
+      throw RepositoryException(message: 'Erro ao buscar produtos');
+    }
+  }
+}

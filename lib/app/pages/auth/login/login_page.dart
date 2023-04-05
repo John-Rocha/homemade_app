86b2918510ac0d484417fa66dcc73a07e79de171
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2023-04-05 12:53:22 -0300

    Tela de login

--- lib/app/pages/auth/login/login_page.dart
+++ lib/app/pages/auth/login/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:homemade_app/app/core/ui/styles/text_styles.dart';
+import 'package:homemade_app/app/core/ui/widgets/delivery_app_bar.dart';
+import 'package:homemade_app/app/core/ui/widgets/delivery_button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,10 +9,72 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+      appBar: DeliveryAppBar(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Login',
+                      style: context.textStyles.textTitle,
+                    ),
+                    const SizedBox(height: 30),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Email'),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Senha'),
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    Center(
+                      child: DeliveryButton(
+                        label: 'Login',
+                        width: double.infinity,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: AlignmentDirectional.bottomCenter,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Não possui uma conta?',
+                      style: context.textStyles.textBold,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Cadastre-se',
+                        style: context.textStyles.textBold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }

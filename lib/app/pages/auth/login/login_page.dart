ae2f9d7b4ba1226f3a52ef8a0b5669077eb4c138
John Rocha <52468127+John-Rocha@users.noreply.github.com>
2023-04-05 20:12:15 -0300

    Validação no login

--- lib/app/pages/auth/login/login_page.dart
+++ lib/app/pages/auth/login/login_page.dart
@@ -2,9 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:homemade_app/app/core/ui/styles/text_styles.dart';
 import 'package:homemade_app/app/core/ui/widgets/delivery_app_bar.dart';
 import 'package:homemade_app/app/core/ui/widgets/delivery_button.dart';
+import 'package:validatorless/validatorless.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailEC = TextEditingController();
+  final _passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailEC.dispose();
+    _passwordEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +33,7 @@ class LoginPage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Form(
+                key: _formKey,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -25,22 +43,34 @@ class LoginPage extends StatelessWidget {
                     ),
                     const SizedBox(height: 30),
                     TextFormField(
+                      controller: _emailEC,
                       decoration: const InputDecoration(
                         label: Text('Email'),
                       ),
+                      validator: Validatorless.multiple([
+                        Validatorless.required('E-mail obrigatório'),
+                        Validatorless.email('E-mail inválido'),
+                      ]),
                     ),
                     const SizedBox(height: 30),
                     TextFormField(
+                      controller: _passwordEC,
                       decoration: const InputDecoration(
                         label: Text('Senha'),
                       ),
+                      validator: Validatorless.required('Senha é obrigatória'),
                     ),
                     const SizedBox(height: 50),
                     Center(
                       child: DeliveryButton(
                         label: 'Login',
                         width: double.infinity,
-                        onPressed: () {},
+                        onPressed: () {
+                          final valid =
+                              _formKey.currentState?.validate() ?? false;
+
+                          if (valid) {}
+                        },
                       ),
                     ),
                   ],
